0a5ccc489ce55f9154aa8e88023c396c95343233
Sir Rufo <oliver.muenzberg@gmail.com>
2022-02-12 21:13:29 +0100

    Projektdateien hinzufügen.

---
+++ CheckedTitle.WpfApp/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace CheckedTitle.WpfApp
+{
+    public class MainViewModel : ReactiveObject
+    {
+        [Reactive] public bool IsBusy { get; set; }
+    }
+}
