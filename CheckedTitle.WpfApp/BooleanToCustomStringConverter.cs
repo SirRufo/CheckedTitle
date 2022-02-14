1d8df10518bcde7981b41072020baa98c2d9688c
Sir Rufo <oliver.muenzberg@gmail.com>
2022-02-14 12:39:42 +0100

    added converter

---
+++ CheckedTitle.WpfApp/BooleanToCustomStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CheckedTitle.WpfApp
+{
+    public class BooleanToCustomConverter : MarkupExtension, IValueConverter
+    {
+        public string? TrueValue { get; set; }
+        public string? FalseValue { get; set; }
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return b ? TrueValue : FalseValue;
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+    }
+}
